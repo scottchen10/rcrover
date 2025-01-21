05e8b82e41cb7faa90b589b13e973c6cbb8edf16
Scott <scottchen227@gmail.com>
2025-01-20 19:26:53 -0500

    Added bugs

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
 
-const struct {
-  uint8_t Left[2] = {HIGH, LOW};
-  uint8_t Right[2] = {LOW, HIGH};
-} MOTOR_FWD_PARAMS;
+const uint8_t MOTOR_BRAKE[2] = {LOW, LOW};
 
-const struct {
-  uint8_t Left[2] = {LOW, HIGH};
-  uint8_t Right[2] = {HIGH, LOW};
-} MOTOR_BWD_PARAMS;
+struct MotorDirectionParams {
+  uint8_t* FWDLeft[2];
+  uint8_t* FWDRight[2];
+  uint8_t* BWDLeft[2];
+  uint8_t* BWDRight[2];
+  
+  MotorDirectionParams(uint8_t lInputA, uint8_t lInputB, uint8_t rInputA, uint8_t rInputB) {
+    (*FWDLeft)[0] = lInputA;
+    (*FWDLeft)[1] = lInputB;
+    (*BWDLeft)[0] = lInputB;
+    (*BWDLeft)[1] = lInputA;
+
+    (*FWDRight)[0] = rInputA;
+    (*FWDRight)[1] = rInputB;
+    (*BWDRight)[0] = rInputB;
+    (*BWDRight)[1] = rInputA;
+  };
+};
 
 const struct{
   int LEnable = 9;
@@ -21,6 +32,8 @@ const struct{
 
 class MotorDriverController{
   public:
+    MotorDirectionParams MotorDirection;
+
     void Initialize() {
       pinMode(MOTOR_DRIVER_PINS.LEnable, OUTPUT);
       pinMode(MOTOR_DRIVER_PINS.LInputA, OUTPUT);
@@ -30,24 +43,42 @@ class MotorDriverController{
       pinMode(MOTOR_DRIVER_PINS.RInputB, OUTPUT);
     };
 
-    void TurnInPlace(bool leftBias) {
-      uint8_t leftDirection;
-      uint8_t rightDirection;
+    void TurnInPlace(bool isLeftTurn) {
+      uint8_t *leftDirection = (isLeftTurn) ? MotorDirection.BWDLeft: MotorDirection.FWDLeft;
+      uint8_t *rightDirection = (isLeftTurn) ? MotorDirection.BWDRight: MotorDirection.FWDRight;
       
+      drive(&leftDirection, 255, &rightDirection, 255);
     }
+
+    void MovingTurn(bool isLeftTurn, bool isFWD) {
+      const uint8_t *leftDirection;
+      const uint8_t *rightDirection;    
+
+      if (isFWD) {
+        
+      }
+
+
+      drive(&leftDirection, &rightDirection);      
+    };
+
+    MotorDriverController(MotorDirectionParams motorDirection) {
+      MotorDirection = motorDirection;
+    };
+
   private:
-    void drive(uint8_t*  leftDirection[2], uint8_t* rightDirection[2]) {
-      analogWrite(MOTOR_DRIVER_PINS.LEnable, 255);
+    void drive(const uint8_t*  leftDirection[2], const uint8_t leftPower, const uint8_t* rightDirection[2], const uint8_t rightPower) {
+      analogWrite(MOTOR_DRIVER_PINS.LEnable, leftPower);
       digitalWrite(MOTOR_DRIVER_PINS.LInputA, (*leftDirection)[0]);
       digitalWrite(MOTOR_DRIVER_PINS.LInputB, (*leftDirection)[1]);
 
-      analogWrite(MOTOR_DRIVER_PINS.REnable, 255);
+      analogWrite(MOTOR_DRIVER_PINS.REnable, rightPower);
       digitalWrite(MOTOR_DRIVER_PINS.RInputA, (*rightDirection)[0]);
       digitalWrite(MOTOR_DRIVER_PINS.RInputB, (*rightDirection)[1]);
-    }
+    };
 };
 
-MotorDriverController MotorDriverControl;
+MotorDriverController MotorDriverControl(MotorDirectionParams(HIGH, LOW, LOW, HIGH));
 
 void setup() {
   MotorDriverControl.Initialize();
