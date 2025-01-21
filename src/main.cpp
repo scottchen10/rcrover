f0ddecb002a3c33905480280a5c4d56ab9d282b3
Scott <scottchen227@gmail.com>
2025-01-21 16:41:36 -0500

    added RF24 dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <Arduino.h>
 #include <MotorDriverController.h>
+#include <RF24.h>
+#include <SPI.h>
+
+const struct {
+  uint8_t CE_PIN = 8;
+  uint8_t CSN_PIN = 7;
+  byte addresses[2][6] = {"00001", "00002"};
+} RADIO;
+
+RF24 radio(RADIO.CE_PIN, RADIO.CSN_PIN);
 
 MotorDriverController MotorControl(
   MotorDirectionSignals()
@@ -12,6 +22,12 @@ MotorDriverController MotorControl(
 
 void setup() {
   MotorControl.Initialize();
+  
+  radio.begin();
+  radio.openWritingPipe(RADIO.addresses[1]);
+  radio.openReadingPipe(1, RADIO.addresses[0]);
+  radio.setPALevel(RF24_PA_MIN);
+
   Serial.begin(115200);
 }
 
