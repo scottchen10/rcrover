63074ca9dd4276dd2638af8378b8bd5278a9c6cd
Scott <scottchen227@gmail.com>
2025-01-21 15:12:23 -0500

    reorganized motordrivercontroller

---
+++ include/MotorDriverController.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <Arduino.h>
+
+struct MotorDirectionSignals {
+  uint8_t FWDLeft[2];
+  uint8_t FWDRight[2];
+  uint8_t BWDLeft[2];
+  uint8_t BWDRight[2];
+
+  uint8_t Brake[2] = {LOW, LOW};
+  
+  MotorDirectionSignals& addLeftFWDSignal(uint8_t lInputA, uint8_t lInputB) {};
+
+  MotorDirectionSignals& addRightFWDSignal(uint8_t rInputA, uint8_t rInputB) {};
+};
+
+struct MotorDriverPins {
+  uint8_t LEnable;
+  uint8_t LInputA;
+  uint8_t LInputB;
+  uint8_t REnable;
+  uint8_t RInputA;
+  uint8_t RInputB;
+
+  MotorDriverPins& addLeftPins(uint8_t LEnable, uint8_t LInputA, uint8_t LInputB) {};
+  MotorDriverPins& addRightPins(uint8_t REnable, uint8_t RInputA, uint8_t RInputB) {};
+};
+
+// Abstraction to drive two motors using the L293D driver for a differentially steered model rover type vehicle. 
+// N20 Micro motors are not powerful enough to run without full power. 
+// In the future I may allow for better control of the power of each.
+class MotorDriverController{
+  public:
+    enum DriveDirection {
+      LEFT,
+      RIGHT,
+      FWD,
+      BWD
+    };
+    MotorDriverPins MotorPins;
+    MotorDirectionSignals MotorDirection;
+
+    void Initialize() {};
+    void TurnInPlace(DriveDirection turnDirection) {}
+    void MovingTurn(DriveDirection turnDirection, DriveDirection lateralDirection) {};
+    void Move(bool lateralDirection) {};
+    MotorDriverController(MotorDirectionSignals motorDirection, MotorDriverPins motorPins) {};
+  private:
+    void drive(const uint8_t*  leftDirection[2], uint8_t leftPower, const uint8_t* rightDirection[2], uint8_t rightPower) {};
+};
---
+++ src/MotorDriverController.cpp
@@ -0,0 +1,90 @@
+#include "MotorDriverController.h"
+
+
+MotorDirectionSignals& MotorDirectionSignals::addLeftFWDSignal(uint8_t lInputA, uint8_t lInputB) {
+    FWDLeft[0] = lInputA;
+    FWDLeft[1] = lInputB;
+    BWDLeft[0] = lInputB;
+    BWDLeft[1] = lInputA;
+
+    return *this;
+};
+
+MotorDirectionSignals& MotorDirectionSignals::addRightFWDSignal(uint8_t rInputA, uint8_t rInputB) {
+    FWDRight[0] = rInputA;
+    FWDRight[1] = rInputB;
+    BWDRight[0] = rInputB;
+    BWDRight[1] = rInputA;
+};
+
+MotorDriverPins& MotorDriverPins::addLeftPins(uint8_t LEnable, uint8_t LInputA, uint8_t LInputB) {
+    this->LEnable = LEnable;
+    this->LInputA = LInputA;
+    this->LInputB = LInputB;
+
+    return *this;
+};
+MotorDriverPins& MotorDriverPins::addRightPins(uint8_t REnable, uint8_t RInputA, uint8_t RInputB) {
+    this->REnable = REnable;
+    this->RInputA = RInputA;
+    this->RInputB = RInputB;
+
+    return *this;
+};
+
+// Abstraction to drive two motors using the L293D driver for a differentially steered model rover type vehicle. 
+// N20 Micro motors are not powerful enough to run without full power. 
+// In the future I may allow for better control of the power of each.
+void MotorDriverController::Initialize() {
+    pinMode(MotorPins.LEnable, OUTPUT);
+    pinMode(MotorPins.LInputA, OUTPUT);
+    pinMode(MotorPins.LInputB, OUTPUT);
+    pinMode(MotorPins.REnable, OUTPUT);
+    pinMode(MotorPins.RInputA, OUTPUT);
+    pinMode(MotorPins.RInputB, OUTPUT);
+};
+
+void MotorDriverController::TurnInPlace(DriveDirection turnDirection) {
+    const uint8_t *leftDirection = (turnDirection == LEFT) ? MotorDirection.BWDLeft: MotorDirection.FWDLeft;
+    const uint8_t *rightDirection = (turnDirection == LEFT) ? MotorDirection.FWDRight: MotorDirection.BWDRight;
+    
+    drive(&leftDirection, 255, &rightDirection, 255);
+}
+
+void MotorDriverController::MovingTurn(DriveDirection turnDirection, DriveDirection lateralDirection) {
+    const uint8_t *leftDirection;
+    const uint8_t *rightDirection;    
+    bool isMovingLeft = turnDirection == LEFT;
+
+    if (lateralDirection == FWD) {
+    leftDirection = (isMovingLeft) ? MotorDirection.Brake: MotorDirection.FWDLeft;
+    rightDirection = (isMovingLeft) ? MotorDirection.FWDRight: MotorDirection.Brake;
+    } else {
+    leftDirection = (isMovingLeft) ? MotorDirection.Brake: MotorDirection.BWDRight;
+    rightDirection = (isMovingLeft) ? MotorDirection.BWDLeft: MotorDirection.Brake;
+    }
+
+    drive(&leftDirection, 255, &rightDirection, 255);      
+};
+
+void MotorDriverController::Move(bool lateralDirection) {
+    const uint8_t *leftDirection = (lateralDirection == FWD) ? MotorDirection.FWDLeft: MotorDirection.BWDRight;
+    const uint8_t *rightDirection = (lateralDirection == FWD) ? MotorDirection.FWDRight: MotorDirection.BWDRight;
+
+    drive(&leftDirection, 255, &rightDirection, 255);   
+}
+
+MotorDriverController::MotorDriverController(MotorDirectionSignals motorDirection, MotorDriverPins motorPins) {
+    MotorPins = motorPins;
+    MotorDirection = motorDirection;
+};
+
+void MotorDriverController::drive(const uint8_t*  leftDirection[2], uint8_t leftPower, const uint8_t* rightDirection[2], uint8_t rightPower) {
+    analogWrite(MotorPins.LEnable, leftPower);
+    digitalWrite(MotorPins.LInputA, (*leftDirection)[0]);
+    digitalWrite(MotorPins.LInputB, (*leftDirection)[1]);
+
+    analogWrite(MotorPins.REnable, rightPower);
+    digitalWrite(MotorPins.RInputA, (*rightDirection)[0]);
+    digitalWrite(MotorPins.RInputB, (*rightDirection)[1]);
+};
